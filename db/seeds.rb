7f73df950d5526abd52caec7eb297c41b65423bc
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-22 22:47:19 +0800

    Successfully seed recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,16 +21,23 @@ puts "Generating ingredients..."
   Ingredient.create(name: Faker::Food.ingredient)
 end
 
-puts "Generating users..."
-User.create(email: 'j@j.j', password: 'jjjjjj', username: 'jro', first_name: 'Jethro', last_name: 'Williams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542027237/stock_photo.jpg', admin: true)
-User.create(email: 'p@p.p', password: 'pppppp', username: 'pro', first_name: 'Pethro', last_name: 'Milliams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542031690/coming_soon.jpg', admin: false)
-18.times do
-  User.create(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, profile_picture: 'https://picsum.photos/200/300', admin: false)
+puts "Generating users and recipes..."
+u1 = User.new(email: 'j@j.j', password: 'jjjjjj', username: 'jro', first_name: 'Jethro', last_name: 'Williams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542027237/stock_photo.jpg', admin: true)
+u1.save
+3.times do
+  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u1.id)
 end
-
-# puts "Generating recipes..."
-# 20.times do
-#   Recipe.create()
-# end
+u2 = User.new(email: 'p@p.p', password: 'pppppp', username: 'pro', first_name: 'Pethro', last_name: 'Milliams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542031690/coming_soon.jpg', admin: false)
+u2.save
+3.times do
+  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u2.id)
+end
+18.times do
+  u3 = User.new(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, profile_picture: 'https://picsum.photos/200/300', admin: false)
+  u3.save
+  2.times do
+    Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u3.id)
+  end
+end
 
 puts "Finished seeding"
