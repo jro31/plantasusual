5df8aba280cbd809211606cb80eebe067ddf57b5
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-23 08:19:49 +0800

    Successfully seed amounts

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
+  has_many :amounts, dependent: :destroy
   has_many :ingredients, through: :amounts
-  has_many :equipments, through: :recipe_equipments
+  has_many :recipe_equipments, dependent: :destroy
+  has_many :equipment, through: :recipe_equipments
   has_many :user_ratings, dependent: :destroy
   has_many :favourites, dependent: :destroy
+  has_many :recipe_categories, dependent: :destroy
+  has_many :categories, through: :recipe_categories
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
+puts "Deleting recipes..."
+Recipe.destroy_all
+
 puts "Deleting equipment..."
 Equipment.destroy_all
 
 puts "Deleting ingredients..."
 Ingredient.destroy_all
 
-puts "Deleting recipes..."
-Recipe.destroy_all
+puts "Deleting categories..."
+Category.destroy_all
 
 puts "Deleting users..."
 User.destroy_all
@@ -20,27 +23,36 @@ puts "Generating ingredients..."
   Ingredient.create(name: Faker::Food.ingredient)
 end
 
+puts "Generating categories..."
+categories_array = ['Dessert', 'Healthy', 'Breakfast', 'Gluten-free', 'Grain', 'Smoothie', 'Main', 'Side', 'Condiment', 'Salad', 'Soup', 'Sandwich']
+categories_array.each do |c|
+  Category.create!(name: c)
+end
+
 puts "Generating users and recipes..."
 u1 = User.new(email: 'j@j.j', password: 'jjjjjj', username: 'jro', first_name: 'Jethro', last_name: 'Williams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542027237/stock_photo.jpg', admin: true)
 u1.save
 3.times do
-  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u1.id)
+  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u1.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
 end
 u2 = User.new(email: 'p@p.p', password: 'pppppp', username: 'pro', first_name: 'Pethro', last_name: 'Milliams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542031690/coming_soon.jpg', admin: false)
 u2.save
 3.times do
-  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u2.id)
+  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u2.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
 end
 18.times do
   u3 = User.new(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, profile_picture: 'https://picsum.photos/200/300', admin: false)
   u3.save
   2.times do
-    Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u3.id)
+    Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://picsum.photos/200/300', user_id: u3.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
   end
 end
 
 users = User.all
 recipes = Recipe.all
+equipments = Equipment.all
+ingredients = Ingredient.all
+categories = Category.all
 
 puts "Generating reviews..."
 50.times do
@@ -54,8 +66,23 @@ end
 
 puts "Generating favourites..."
 42.times do |n|
-  Favourite.create!(recipe_id: recipes[n].id, user_id: users[rand(0..9)].id, star: true)
+  Favourite.create!(recipe_id: recipes[n].id, user_id: users[rand(0..10)].id, star: true)
   Favourite.create!(recipe_id: recipes[n].id, user_id: users[rand(10...20)].id, star: true)
 end
+
+puts "Generating amounts..."
+prep_methods = ['chopped', 'seeded', 'rinsed', 'quartered', 'diced', 'toasted', 'ground', 'shelled']
+202.times do
+ Amount.create!(ingredient_id: ingredients.sample.id, recipe_id: recipes.sample.id, size: rand(1...100), unit: Faker::Food.metric_measurement, preparation: prep_methods.sample, optional: false)
+end
+50.times do
+  Amount.create!(ingredient_id: ingredients.sample.id, recipe_id: recipes.sample.id, size: rand(1...100), unit: Faker::Food.metric_measurement, preparation: prep_methods.sample, optional: true)
+end
+
+puts "Generating recipe equipment..."
+
+
+puts "Generating recipe categories..."
+
 
 puts "Finished seeding"
