8cda203bc3cd08dbf43e236b9e0d94d1a4e71b6e
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-23 21:47:26 +0800

    Add some visualisation to the homepage and create the recipes#index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
   has_many :favourites
   has_many :user_ratings
 
-  validates :username, presence: true, length: { minimum: 3, maximum: 100 }, uniqueness: true
+  validates :username, presence: true, uniqueness: true, format: { with: /\w{3,16}/, message: "must be 3 to 16 characters, of letters, numbers and underscores only" }
+  validates :first_name, presence: false, format: { with: /[a-zA-Z]{2,50}/, message: "must use letters only" }
+  validates :last_name, presence: false, format: { with: /[a-zA-Z]{2,50}/, message: "must use letters only" }
 end
