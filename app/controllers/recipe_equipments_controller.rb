bbb5147467d089faabffb090afab384e03d01994
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-30 22:28:19 +0000

    Complete the back-end for adding a recipe (with the exception of uploading a photo/photos) by allowing users to write a method and add the number of servings and the cooking time. The recipe show page now displays all of this information.

--- app/controllers/recipe_equipments_controller.rb
+++ app/controllers/recipe_equipments_controller.rb
@@ -1,5 +1,20 @@
 class RecipeEquipmentsController < ApplicationController
   def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_equipment = RecipeEquipment.new
+  end
 
+  def create
+    selected_equipments = params[:recipe_equipment][:equipment_id]
+    selected_equipments.each do |equipment|
+      if equipment != ""
+        selected_equipment = { "equipment_id" => equipment }
+        @recipe_equipment = RecipeEquipment.new(selected_equipment)
+        @recipe = Recipe.find(params[:recipe_id])
+        @recipe_equipment.recipe = @recipe
+        @recipe_equipment.save
+      end
+    end
+    redirect_to edit_recipe_path(@recipe)
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,25 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @amounts = Amount.where(recipe_id: params[:id])
+    @categories = RecipeCategory.where(recipe_id: params[:id])
+    @equipments = RecipeEquipment.where(recipe_id: params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @times = []
+    180.times do |n|
+      if n % 5 == 0
+        @times << n + 5
+      end
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
   end
 end
 
