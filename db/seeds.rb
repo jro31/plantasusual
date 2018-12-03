6d02c721965be19a5b19c955fd0df4d427e9e75f
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-03 16:13:56 +0000

    Update the seeds file to (hopefully) work once pushed to production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-puts "Deleting recipes..."
-Recipe.destroy_all
+# puts "Deleting recipes..."
+# Recipe.destroy_all
 
 puts "Deleting equipment..."
 Equipment.destroy_all
 
-puts "Deleting ingredients..."
-Ingredient.destroy_all
+# puts "Deleting ingredients..."
+# Ingredient.destroy_all
 
 puts "Deleting categories..."
 Category.destroy_all
 
-puts "Deleting users..."
-User.destroy_all
+# puts "Deleting users..."
+# User.destroy_all
 
 puts "Deleting preparation methods"
 PreparationMethod.destroy_all
@@ -20,93 +20,93 @@ puts "Deleting units"
 Unit.destroy_all
 
 puts "Generating equipment..."
-equipment_array = ['blender', 'food processor', 'oven', 'stove', 'skillet', 'baking dish']
+equipment_array = ['blender', 'food processor', 'oven', 'stove', 'skillet', 'baking dish', 'grill', 'mixing bowl', 'muffin tray', 'cake tin', 'saucepan']
 equipment_array.sort.each do |e|
   Equipment.create(name: e)
 end
 
-puts "Generating ingredients..."
-50.times do
-  Ingredient.create(name: Faker::Food.ingredient.downcase)
-end
+# puts "Generating ingredients..."
+# 50.times do
+#   Ingredient.create(name: Faker::Food.ingredient.downcase)
+# end
 
 puts "Generating categories..."
-categories_array = ['Dessert', 'Healthy', 'Breakfast', 'Gluten-free', 'Grain', 'Smoothie', 'Main', 'Side', 'Condiment', 'Salad', 'Soup', 'Sandwich'].sort
+categories_array = ['dessert', 'healthy', 'breakfast', 'gluten-free', 'grain', 'smoothie', 'main', 'side', 'condiment', 'salad', 'soup', 'sandwich', 'fast food', 'snack', 'paleo', 'stew', 'pasta', 'curry', 'baked']
 categories_array.sort.each do |c|
   Category.create!(name: c)
 end
 
-puts "Generating users and recipes..."
-u1 = User.new(email: 'j@j.j', password: 'jjjjjj', username: 'jro', first_name: 'Jethro', last_name: 'Williams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542027237/stock_photo.jpg', admin: true)
-u1.save
-3.times do
-  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://res.cloudinary.com/dftybtoej/image/upload/v1543645966/xwxdflrs0w2wwqjqvtsh.jpg', user_id: u1.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
-end
-u2 = User.new(email: 'p@p.p', password: 'pppppp', username: 'pro', first_name: 'Pethro', last_name: 'Milliams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542031690/coming_soon.jpg', admin: false)
-u2.save
-3.times do
-  Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, user_id: u2.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
-end
-18.times do
-  u3 = User.new(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, profile_picture: 'https://picsum.photos/200/300', admin: false)
-  u3.save
-  2.times do
-    Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://res.cloudinary.com/dftybtoej/image/upload/v1543645966/xwxdflrs0w2wwqjqvtsh.jpg', user_id: u3.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
-  end
-end
+# puts "Generating users and recipes..."
+# u1 = User.new(email: 'j@j.j', password: 'jjjjjj', username: 'jro', first_name: 'Jethro', last_name: 'Williams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542027237/stock_photo.jpg', admin: true)
+# u1.save
+# 3.times do
+#   Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://res.cloudinary.com/dftybtoej/image/upload/v1543645966/xwxdflrs0w2wwqjqvtsh.jpg', user_id: u1.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
+# end
+# u2 = User.new(email: 'p@p.p', password: 'pppppp', username: 'pro', first_name: 'Pethro', last_name: 'Milliams', profile_picture: 'https://res.cloudinary.com/dftybtoej/image/upload/v1542031690/coming_soon.jpg', admin: false)
+# u2.save
+# 3.times do
+#   Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, user_id: u2.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
+# end
+# 18.times do
+#   u3 = User.new(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, profile_picture: 'https://picsum.photos/200/300', admin: false)
+#   u3.save
+#   2.times do
+#     Recipe.create!(name: Faker::Food.dish, method: Faker::Food.description, photo: 'https://res.cloudinary.com/dftybtoej/image/upload/v1543645966/xwxdflrs0w2wwqjqvtsh.jpg', user_id: u3.id, servings: rand(1...8), cooking_time: (rand(1...24) * 5))
+#   end
+# end
 
 puts "Generating preparation methods..."
-prep_methods = ['chopped', 'seeded', 'rinsed', 'quartered', 'diced', 'toasted', 'ground', 'shelled']
+prep_methods = ['chopped', 'seeded', 'rinsed', 'quartered', 'diced', 'toasted', 'ground', 'shelled', 'soaked', 'dried']
 prep_methods.sort.each do |pm|
   PreparationMethod.create!(name: pm)
 end
 
 puts "Generating units..."
-units = ['grams', 'millilitres', 'pinches', 'splashes', 'teaspoons', 'tablespoons', 'cups', 'dashes', 'litres', 'kilograms', 'pieces']
+units = ['grams', 'millilitres', 'pinch', 'splash', 'teaspoons', 'tablespoons', 'cups', 'dash', 'litres', 'kilograms', 'pieces', 'inches', 'centimetres', 'cans', 'packs']
 units.sort.each do |unit|
   Unit.create!(measurement: unit)
 end
 
-users = User.all
-recipes = Recipe.all
-equipments = Equipment.all
-ingredients = Ingredient.all
-categories = Category.all
-preparation_methods = PreparationMethod.all
-units = Unit.all
+# users = User.all
+# recipes = Recipe.all
+# equipments = Equipment.all
+# ingredients = Ingredient.all
+# categories = Category.all
+# preparation_methods = PreparationMethod.all
+# units = Unit.all
 
-puts "Generating comments..."
-50.times do
-  Comment.create!(body: Faker::GreekPhilosophers.quote, user_id: users.sample.id, recipe_id: recipes.sample.id)
-end
+# puts "Generating comments..."
+# 50.times do
+#   Comment.create!(body: Faker::GreekPhilosophers.quote, user_id: users.sample.id, recipe_id: recipes.sample.id)
+# end
 
-puts "Generating favourites..."
-42.times do |n|
-  Favourite.create!(recipe_id: recipes[n].id, user_id: users[rand(0..10)].id)
-  Favourite.create!(recipe_id: recipes[n].id, user_id: users[rand(10...20)].id)
-end
+# puts "Generating favourites..."
+# 42.times do |n|
+#   Favourite.create!(recipe_id: recipes[n].id, user_id: users[rand(0..10)].id)
+#   Favourite.create!(recipe_id: recipes[n].id, user_id: users[rand(10...20)].id)
+# end
 
-puts "Generating amounts..."
-202.times do
- Amount.create!(ingredient_id: ingredients.sample.id, recipe_id: recipes.sample.id, size: rand(1...100), unit_id: units.sample.id, preparation_method_id: preparation_methods.sample.id, optional: false)
-end
-50.times do
-  Amount.create!(ingredient_id: ingredients.sample.id, recipe_id: recipes.sample.id, size: rand(1...100), unit_id: units.sample.id, preparation_method_id: preparation_methods.sample.id, optional: true)
-end
+# puts "Generating amounts..."
+# 202.times do
+#  Amount.create!(ingredient_id: ingredients.sample.id, recipe_id: recipes.sample.id, size: rand(1...100), unit_id: units.sample.id, preparation_method_id: preparation_methods.sample.id, optional: false)
+# end
+# 50.times do
+#   Amount.create!(ingredient_id: ingredients.sample.id, recipe_id: recipes.sample.id, size: rand(1...100), unit_id: units.sample.id, preparation_method_id: preparation_methods.sample.id, optional: true)
+# end
 
-puts "Generating recipe equipment..."
-50.times do
-  RecipeEquipment.create!(recipe_id: recipes.sample.id, equipment_id: equipments.sample.id)
-end
+# puts "Generating recipe equipment..."
+# 50.times do
+#   RecipeEquipment.create!(recipe_id: recipes.sample.id, equipment_id: equipments.sample.id)
+# end
 
-puts "Generating recipe categories..."
-75.times do
-  RecipeCategory.create!(recipe_id: recipes.sample.id, category_id: categories.sample.id)
-end
+# puts "Generating recipe categories..."
+# 75.times do
+#   RecipeCategory.create!(recipe_id: recipes.sample.id, category_id: categories.sample.id)
+# end
 
-puts "Generating profiles"
-users.each do |user|
-  Profile.create!(user_id: user.id)
-end
+# puts "Generating profiles"
+# users.each do |user|
+#   Profile.create!(user_id: user.id)
+# end
 
 puts "Finished seeding"
