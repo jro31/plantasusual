032fcb21481cf545c8f2b81fa0eb3323ad1fddb8
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-03 13:11:38 +0000

    Make the recipes#index page the homepage for signed-in users, and move the flashes above the logo and search so that they always appear at the top of the screen.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root 'recipes#index', as: :authenticated_root
+  end
+
   root to: 'pages#home'
 
   resources :profiles, only: [:show]
