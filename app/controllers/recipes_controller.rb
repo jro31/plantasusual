b5bba4f240c4714e95f2ab4da4deb575f39705d3
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-04 20:08:40 +0000

    Simplify the search bar to something that actually... works

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,15 +20,8 @@ class RecipesController < ApplicationController
 
   def index
     if params[:query].present?
-      sql_query = " \
-        recipes.name ILIKE :query \
-        OR recipes.method ILIKE :query \
-        OR users.username ILIKE :query \
-        OR ingredients.name ILIKE :query \
-        OR equipment.name ILIKE :query \
-        OR categories.name ILIKE :query \
-        "
-      @recipes = Recipe.joins(:user, :ingredients, :equipment, :categories).where(sql_query, query: "%#{params[:query]}%").order(:name)
+      sql_query = "name ILIKE :query OR method ILIKE :query"
+      @recipes = Recipe.where(sql_query, query: "%#{params[:query]}%")
     else
       @recipes = Recipe.all.order(:name)
     end
