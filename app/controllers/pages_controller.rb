1538acb2794fcd1f89e5886e70f9ecd20ae5fe1a
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-10 10:09:40 +0000

    Add user list, recipe list, comment list, recipe report list and comment report list to the admin panel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,10 @@ class PagesController < ApplicationController
     @search_bar_hide = true
     @recipe_reports = policy_scope(RecipeReport).order(created_at: :desc)
     @comment_reports = policy_scope(CommentReport).order(created_at: :desc)
+    @users = User.all.order(:id)
+    @recipes = Recipe.all.order(:id)
+    @comments = Comment.all.order(:id)
+    @recipe_reports_all = RecipeReport.all.order(:id)
+    @comment_reports_all = CommentReport.all.order(:id)
   end
 end
