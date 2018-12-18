ab9a97769881ae66eceed7225f9af9c27745e0ab
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-18 14:58:14 +0000

    Add uniqueness validation to related recipes

--- app/models/recipe_relate.rb
+++ app/models/recipe_relate.rb
@@ -1,4 +1,6 @@
 class RecipeRelate < ApplicationRecord
   belongs_to :relater, foreign_key: "relater_id", class_name: "Recipe"
   belongs_to :relatee, foreign_key: "relatee_id", class_name: "Recipe"
+
+  validates :relater, uniqueness: { scope: :relatee }
 end
