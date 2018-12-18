32f77c671664a7f706abf9a729cddc7f674c3ab7
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-18 15:32:14 +0000

    Create private messages model and update the visualized schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_225436) do
+ActiveRecord::Schema.define(version: 2018_12_18_152807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,15 @@ ActiveRecord::Schema.define(version: 2018_12_16_225436) do
 
   create_table "preparation_methods", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "private_messages", force: :cascade do |t|
+    t.string "topic"
+    t.text "body"
+    t.integer "sender_id"
+    t.integer "receiver_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
