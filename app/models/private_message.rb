00e1d0bb3f17eccdff6925433b0591e113e19b71
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-18 18:45:03 +0000

    Remove 'topic' column from private_message table and update visualized schema

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -2,5 +2,5 @@ class PrivateMessage < ApplicationRecord
   belongs_to :sender, foreign_key: "sender_id", class_name: "User"
   belongs_to :receiver, foreign_key: "receiver_id", class_name: "User"
 
-  validates :topic, presence: true
+  validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_152807) do
+ActiveRecord::Schema.define(version: 2018_12_18_184127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,7 +103,6 @@ ActiveRecord::Schema.define(version: 2018_12_18_152807) do
   end
 
   create_table "private_messages", force: :cascade do |t|
-    t.string "topic"
     t.text "body"
     t.integer "sender_id"
     t.integer "receiver_id"
