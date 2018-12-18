b41f84c1fd178625317abe7d98b226ed3e75e4df
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-18 15:46:41 +0000

    Complete the has many/belongs to relationship between user and private message

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -1,2 +1,4 @@
 class PrivateMessage < ApplicationRecord
+  belongs_to :sender, foreign_key: "sender_id", class_name: "User"
+  belongs_to :receiver, foreign_key: "receiver_id", class_name: "User"
 end
